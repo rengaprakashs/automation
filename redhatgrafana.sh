a7753b87c1557ecb3f63c9355bd1d8fa7606aaad
root <root@ip-172-31-44-39.ap-south-1.compute.internal>
2020-11-21 03:30:17 +0000

    renga

---
+++ redhatgrafana.sh
@@ -0,0 +1,14 @@
+cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+yum install grafana -y
+sudo systemctl enable --now grafana-server.service 
